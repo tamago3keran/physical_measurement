3d1554723cbdb65fc8daa75e5b2274dd615d0d08
tamago3keran <yamataku3831@gmail.com>
2020-06-21 20:01:51 +0900

    use SharedPreferences

--- app/src/main/java/com/example/physicalmeasurement/MainActivity.kt
+++ app/src/main/java/com/example/physicalmeasurement/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.physicalmeasurement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
+import androidx.preference.PreferenceManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editNeck = pref.getString("NECK", "")
+        val editSleeve = pref.getString("SLEEVE", "")
+        val editWaist = pref.getString("WAIST", "")
+        val editInseam = pref.getString("INSEAM", "")
+
+        neck.setText(editNeck)
+        sleeve.setText(editSleeve)
+        waist.setText(editWaist)
+        inseam.setText(editInseam)
+
+        save.setOnClickListener { onSaveTapped() }
+    }
+
+    private fun onSaveTapped() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        pref.edit {
+            putString("NECK", neck.text.toString())
+            putString("SLEEVE", sleeve.text.toString())
+            putString("WAIST", waist.text.toString())
+            putString("INSEAM", inseam.text.toString())
+        }
     }
 }
